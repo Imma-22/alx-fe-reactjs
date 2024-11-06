8fef68a940e18aff557514bbaf8e10344881c720
Imma <immaculata222@gmail.com>
2024-11-06 11:53:10 +0100

    Updated Files

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,16 +1,30 @@
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
 import { useState } from 'react'
 import './App.css'
 
 function Counter() {
   const [count, setCount] = useState(0);
-  
+
   return (
-    <div>
+
+    <>
+      <div>
+      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    
       <p>Current Count: {count}</p>
       <button onClick={() => setCount(count + 1)}>Increment</button>
       <button onClick={() => setCount(count - 1)}>Decrement</button>
       <button onClick={() => setCount(0)}>Reset</button>
     </div>
+    </>
   );
 }
 
