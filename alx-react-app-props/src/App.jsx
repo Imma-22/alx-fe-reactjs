70f28300c60d73b07ae7ff5b300027c0c232fc62
Imma <immaculata222@gmail.com>
2024-11-07 22:18:06 +0100

    Added Files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,15 +2,17 @@
 import React from 'react';
 import ProfilePage from './components/ProfilePage';
 import UserContext from './components/UserContext';
+import UserProfile from './components/UserProfile';
 import './App.css'
 
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const userData = { name: "Jolie Alice", email: "Alice@gmail.com" };
 
   return (
        
       <UserContext.Provider value={userData}>
       <ProfilePage/>
+      <UserProfile/>
       </UserContext.Provider>
     
   );
---
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+const UserProfile = ({ name, age, bio }) => {
+    return (
+
+<div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
+   <h2 style={{ color: 'blue' }}>{name}</h2>
+   <p>Age: 26 <span style={{ fontWeight: 'bold' }}>{age}</span></p>
+   <p>Bio: Loves travel and photography {bio}</p>
+ </div>
+
+);
+};
+
+export default UserProfile;
