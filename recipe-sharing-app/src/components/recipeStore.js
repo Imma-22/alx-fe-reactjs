296d0ae93ac8681eb58431fbb5a18ad2e85782cd
Imma <immaculata222@gmail.com>
2024-11-12 14:01:34 +0100

    Updated files

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,8 @@ import {create} from 'zustand';
 const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+
   updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
