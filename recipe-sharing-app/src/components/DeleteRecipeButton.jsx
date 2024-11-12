34a266e3e74aebae048ad94c57af239528b21ce2
Imma <immaculata222@gmail.com>
2024-11-12 14:09:44 +0100

    Updated files

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,11 +1,14 @@
+import { useNavigate } from 'react-router-dom';
 import { useRecipeStore } from '../components/recipeStore';
 import PropTypes from 'prop-types';
 
 const DeleteRecipeButton = ({ recipeId }) => {
   const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+  const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
+    navigate('/'); // Redirect to the home page after deletion
   };
 
   return (
