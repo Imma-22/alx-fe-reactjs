2dc4e152fa7b62a1dc0f0f299088d8bac8341f43
Imma <immaculata222@gmail.com>
2024-11-19 16:24:14 +0100

    Modified  Files

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import RegistrationForm from './components/RegistrationForm';
-import FormikForm from './components/FormikForm';
+import FormikForm from './components/formikForm';
 import './App.css'
 
 
