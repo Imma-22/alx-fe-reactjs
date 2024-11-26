c60ba86915ae38a2a0d4cdcd672cfaad22c706c1
Imma <immaculata222@gmail.com>
2024-11-27 03:41:29 +0800

    Updated Files

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,10 +6,10 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-36 h-36 sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto"
+        className="md:w-36h-36 sm:w-24h-24 md:w-36 md:h-36 rounded-full mx-auto"
       />
-      <h1 className="text-xl md:text-xl text-blue-800 my-4 text-center">John Doe</h1>
-      <p className="text-gray-600 text-base text-center">
+      <h1 className="sm:text-lg md:text-xl text-blue-800 my-4 text-center">John Doe</h1>
+      <p className="text-gray-600 sm:text-sm md:text-base text-center">
         Developer at Example Co. Loves to write code and explore new
         technologies.
       </p>
