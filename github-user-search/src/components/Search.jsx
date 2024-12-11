7616a3b61a5f643fb7f2a2f0c7993008acae075a
Imma <immaculata222@gmail.com>
2024-12-11 13:53:45 +0100

    Modified  files

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -3,10 +3,13 @@ import { fetchUserData } from "../services/githubService";
 
 function Search({ setUserData, setLoading, setError }) {
   const [username, setUsername] = useState("");
+  const [location, setLocation] = useState("");
+  const [minRepos, setMinRepos] = useState(0);
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (!username) {
+    if (!username && !location && !minRepos) {
       return;
     }
 
@@ -14,7 +17,7 @@ function Search({ setUserData, setLoading, setError }) {
     setError(""); // Reset the error message
     setUserData(null); // Clear previous user data
 
-    const data = await fetchUserData(username);
+    const data = await fetchUserData(username, location, minRepos);
     
     if (data) {
       // Only set user data if the data is successfully fetched
@@ -28,6 +31,8 @@ function Search({ setUserData, setLoading, setError }) {
 
     setLoading(false);
     setUsername(""); // Reset the input field after the search
+    setLocation(""); // Reset the location field
+    setMinRepos(0); // Reset min repos field
   };
 
   return (
@@ -39,6 +44,20 @@ function Search({ setUserData, setLoading, setError }) {
           onChange={(e) => setUsername(e.target.value)}
           className="px-4 py-2 border rounded-md"
           placeholder="Enter GitHub username"
+        />
+        <input
+          type="text"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          className="px-4 py-2 border rounded-md mt-2"
+          placeholder="Location (optional)"
+        />
+        <input
+          type="number"
+          value={minRepos}
+          onChange={(e) => setMinRepos(e.target.value)}
+          className="px-4 py-2 border rounded-md mt-2"
+          placeholder="Min Repositories"
         />
         <button 
           type="submit" 
@@ -54,13 +73,15 @@ function Search({ setUserData, setLoading, setError }) {
 
       {setUserData && !setLoading && !setError && (
         <div className="user-info mt-4">
-          {/* Display the user's avatar and login */}
+          {/* Display the user's avatar, login and additional info  */}
           <img
             src={setUserData.avatar_url}
             alt={setUserData.login}
             className="w-24 h-24 rounded-full border-2 border-gray-300 mx-auto"
           />
           <p className="text-xl font-semibold mt-2">{setUserData.login}</p>
+          <p className="text-gray-500">Location: {setUserData.location}</p>
+          <p className="text-gray-500">Repositories: {setUserData.public_repos}</p>
         </div>
       )}
     </div>
